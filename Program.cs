205d55fbe0c40207a7282e5c41fae41eecc76d67
ariciiamaga <ariciia@live.com>
2024-05-22 16:15:23 -0300

    Dasafio celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia :");
+Smartphone nokia = new Nokia(numero: "8176543", modelo: "Xiaomi", imei: "123456", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "8165432", modelo: "14 pro Max", imei: "87654", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("linkId");
